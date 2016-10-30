3cb62dbac14892ea02efcac20a66065fcda607e0
Matthew Kuzdal <mkuzdal@gmail.com>
2016-10-30 02:47:39 -0700

    Created basic overlay and buttons for main menu screen

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+
+	public Button startText;
+	public Button optionsText;
+	public Button creditsText;
+	public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+
+		startText = startText.GetComponent<Button> ();
+		optionsText = startText.GetComponent<Button> ();
+		creditsText = startText.GetComponent<Button> ();
+		exitText = startText.GetComponent<Button> ();
+
+		quitMenu.enabled = false;
+	}
+		
+	public void PlayPress() {
+		// Application.LoadLevel(1);
+	}
+
+	public void OptionsPress() {
+		
+	}
+
+	public void CreditsPress() {
+
+	}
+	
+	public void ExitPress() {
+		quitMenu.enabled = true;
+
+		startText.enabled = false;
+		optionsText.enabled = false;
+		creditsText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void YesPress() {
+		Application.Quit ();
+	}
+
+	public void NoPress() {
+		quitMenu.enabled = false;
+
+		startText.enabled = true;
+		optionsText.enabled = true;
+		creditsText.enabled = true;
+		exitText.enabled = true;
+	}
+		
+}
