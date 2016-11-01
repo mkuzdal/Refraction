a65c4cfe7b6bbf992930724e38bca45c809ef737
Matthew Kuzdal <mkuzdal@gmail.com>
2016-10-31 22:54:58 -0700

    Planet Graphics

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, rotationSpeed, 0));	
+	}
+}
